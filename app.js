44294143ae9f8e2f0c376243e563966302b5e3c7
cheet <29075792+ImmaCheetah@users.noreply.github.com>
2024-11-20 14:45:37 -0500

    start: create basic set up
    - Create session with prisma
    - Create database for app
    - Add views and public folder
    - Create basic express app

---
+++ app.js
@@ -0,0 +1,68 @@
+require('dotenv').config();
+
+const express = require('express');
+const passport = require('passport');
+const path = require("node:path");
+
+// Prisma session store packages
+const expressSession = require('express-session');
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient({log: ['query']})
+
+// Initialize app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Path to css file
+const assetsPath = path.join(__dirname, "/public");
+
+app.set("views", path.join(__dirname, "views/pages"));
+app.set("view engine", "ejs");
+
+app.use(
+  expressSession({
+    cookie: {
+     maxAge: 7 * 24 * 60 * 60 * 1000 // ms
+    },
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(
+      new PrismaClient(),
+      {
+        checkPeriod: 2 * 60 * 1000,  //ms
+        dbRecordIdIsSessionId: true,
+        dbRecordIdFunction: undefined,
+      }
+    )
+  })
+);
+
+app.use(passport.session());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(assetsPath));
+
+app.get("/", (req, res) => res.send("Hello, world!"));
+
+app.listen(process.env.PORT, () => console.log('App running on port', PORT));
+
+async function main() {
+  // const session = prisma.session.findFirst({
+  //   include: {
+  //     cookie: true
+  //   }
+  // })
+  // console.log(session)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
