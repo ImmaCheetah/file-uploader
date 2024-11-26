51ef11cd7c8e6837c73913315d7a530738b068fb
cheet <29075792+ImmaCheetah@users.noreply.github.com>
2024-11-26 15:22:08 -0500

    feat: add form to upload file and store locally

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,15 +1,23 @@
-const db = require('../db/queries')
+const db = require('../db/queries');
+// const multer  = require('multer')
+// const upload = multer({ dest: 'files/' })
 
 async function getIndexPage(req, res, next) {
   if (!req.user) {
     res.redirect('/login')
   } else {
-    res.send('hello');
+    res.render('index');
   }
   // console.log(req.session);
+}
+
+async function uploadFile(req, res, next) {
+  res.send('done did it');
+  console.log('This is the file', req.file);
 }
 
 
 module.exports = {
   getIndexPage,
+  uploadFile
 }
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,7 +2,11 @@ const { Router } = require("express");
 const indexRouter = Router();
 const indexController = require('../controllers/indexController');
 const passport = require('passport');
+const multer  = require('multer')
+const upload = multer({ dest: 'files/' })
 
 indexRouter.get('/', indexController.getIndexPage);
+
+indexRouter.post('/', upload.single('file'), indexController.uploadFile);
 
 module.exports = indexRouter;
